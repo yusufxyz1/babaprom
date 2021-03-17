e8f085bd06be3149dbf68317e5f95f5c541ee1a3
Glitch (hello-express) <none>
2021-03-17 06:52:26 +0000

    👓🍍 Checkpoint ./komutlar/yardım.js:27665798/4680 ./events/ready.js:27665798/46

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const moment = require('moment');
 const chalk = require('chalk');
-const { prefix } = require('../ayarlar.json')
 
 module.exports = client => {
   var degisenOynuyor = [
