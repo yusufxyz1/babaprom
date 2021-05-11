dd3f367c3c102aa78923712620e8b2fe4674f2d5
Glitch (hello-express) <none>
2021-05-11 13:41:46 +0000

    🍜🗾 Checkpoint ./komutlar/istatistik.js:31738350/1947 ./komutlar/yardım.js:31738350/305

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -12,23 +12,23 @@ const nobles = new Discord.MessageEmbed()// fwhy code dan alınıp editlenmişti
 
 ▬▬▬▬▬▬[ **yDarK BOT | Eğlence Komutları** ]▬▬▬▬▬▬
 
-> **» ${process.env.prefix}adamasmaca :** Adam Asmaca Oynarsınız.
-> **» ${process.env.prefix}gay :** Etiketlediğiniz Kişinin Gaylik Düzeyini Öğrenirsiniz.
-> **» ${process.env.prefix}yılbaşı :** Yılbaşı Sayacı.
-> **» ${process.env.prefix}kaçcm :** :D?
-> **» ${process.env.prefix}kimlik :** Rastgele Kimlik Oluşturursunuz.
-> **» ${process.env.prefix}liderlik :** Coin Sıralamasını Gösterir.
-> **» ${process.env.prefix}balık-tut :** Balık Tutarsınız.
-> **» ${process.env.prefix}trump :** Trump Tweet Atar.
-> **» ${process.env.prefix}rozet :** Rozetlerinizi Gösterir.
-> **» ${process.env.prefix}evlenme-teklifi :** Evlenme Teklifi Edersin. ^^
-> **» ${process.env.prefix}kralol :** Kral Olursun.
-> **» ${process.env.prefix}yazı-tura :** Yazı mı Tura mı?
-> **» ${process.env.prefix}öp :** Birini Öpersin.
-> **» ${process.env.prefix}duello :** Birisiyle Duello Atarsın.
+**| ${process.env.prefix}adamasmaca | :** Adam Asmaca Oynarsınız.
+**| ${process.env.prefix}gay | :** Etiketlediğiniz Kişinin Gaylik Düzeyini Öğrenirsiniz.
+**| ${process.env.prefix}yılbaşı :** Yılbaşı Sayacı.
+**| ${process.env.prefix}kaçcm :** :D?
+**| ${process.env.prefix}kimlik :** Rastgele Kimlik Oluşturursunuz.
+**| ${process.env.prefix}liderlik :** Coin Sıralamasını Gösterir.
+**| ${process.env.prefix}balık-tut :** Balık Tutarsınız.
+**| ${process.env.prefix}trump :** Trump Tweet Atar.
+**| ${process.env.prefix}rozet :** Rozetlerinizi Gösterir.
+**| ${process.env.prefix}evlenme-teklifi :** Evlenme Teklifi Edersin. ^^
+**| ${process.env.prefix}kralol :** Kral Olursun.
+**| ${process.env.prefix}yazı-tura :** Yazı mı Tura mı?
+**| ${process.env.prefix}öp :** Birini Öpersin.
+**| ${process.env.prefix}duello :** Birisiyle Duello Atarsın.
 
 
-**Linkler ->** **[Destek Sunucusu](${ayarlar.destekSunucusuDavetLinki})** **•** **[Botun Davet Linki](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)**`)
+**Linkler ->** **[Destek Sunucusu](${ayarlar.destekSunucusuDavetLinki})** **•** **[Botun Davet Linki](https://discord.com/oauth2/authorize?client_id=767317246119903243&scope=bot&permissions=805314622)**`)
  
 .setFooter(client.user.username + "", client.user.avatarURL)
 .setTimestamp();
