b2c4773259d3ac7d4309e3e963855b6263fa03a0
Glitch (hello-express) <none>
2021-06-03 10:53:01 +0000

    📼🍃 Checkpoint ./komutlar/kral-ol.js:31738350/88 ./komutlar/kullanıcı-bilgi.js:31738350/8541

--- komutlar/kullanıcı-bilgi.js
+++ komutlar/kullanıcı-bilgi.js
@@ -1,45 +1,93 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
-const moment = require("moment");
-const ayarlar = require('../ayarlar.json');
+const Discord = require('discord.js')
+const moment = require('moment')
+const client = new Discord.Client();
 
-exports.run = async (client, message, args, tools) => {
-let user;
+const botadi = "yDarK BOT"
 
-  if (message.mentions.users.first()) {
-      user = message.mentions.users.first();
-    } else {
-        user = message.author;
+exports.run = async (bot, msg, args) => {
+        let simdikitarih = moment.utc(msg.createdAt).format('DD MM YYYY');
+
+        let user = msg.mentions.users.first() || msg.author;
+
+        let userinfo = {};
+        userinfo.avatar= user.displayAvatarURL;
+        userinfo.id = user.id;
+        userinfo.od1 = msg.guild.members.get(user.id).user.presence.game || "Oynadığı bir oyun yok"
+        userinfo.status = user.presence.status.toString()
+        .replace("dnd", `Rahatsız Etmeyin`)
+        .replace("online", `Çevrimiçi`)
+        .replace("idle", `Boşta`)
+        .replace("offline", `Çevrimdışı`)
+        userinfo.bot = user.bot.toString()
+        .replace("false", `Hayır`)
+        .replace("true", `Evet`)
+        userinfo.sonmesaj = user.lastMessage || "Son yazılan mesaj bulunamadı." || "Son yazılan mesaj gösterilemedi."
+
+        userinfo.dctarih = moment.utc(msg.guild.members.get(user.id).user.createdAt).format('**YYYY** [Yılında] MMMM [Ayında] dddd [Gününde] (**DD/MM/YYYY**)')
+
+        .replace("Monday", `**Pazartesi**`)
+        .replace("Tuesday", `**Salı**`)
+        .replace("Wednesday", `**Çarşamba**`)
+        .replace("Thursday", `**Perşembe**`)
+        .replace("Friday", `**Cuma**`)
+        .replace("Saturday", `**Cumartesi**`)
+        .replace("Sunday", `**Pazar**`)
+        .replace("January", `**Ocak**`)
+        .replace("February", `**Şubat**`)
+        .replace("March", `**Mart**`)
+        .replace("April", `**Nisan**`)
+        .replace("May", `**Mayıs**`)
+        .replace("June", `**Haziran**`)
+        .replace("July", `**Temmuz**`)
+        .replace("August", `**Ağustos**`)
+        .replace("September", `**Eylül**`)
+        .replace("October", `**Ekim**`)
+        .replace("November", `**Kasım**`)
+        .replace("December", `**Aralık**`)
+        userinfo.dctarihkatilma = moment.utc(msg.guild.members.get(user.id).joinedAt).format('**YYYY** [Yılında] MMMM [Ayında] dddd [Gününde] (**DD/MM/YYYY**)')
+        .replace("Monday", `**Pazartesi**`)
+        .replace("Tuesday", `**Salı**`)
+        .replace("Wednesday", `**Çarşamba**`)
+        .replace("Thursday", `**Perşembe**`)
+        .replace("Friday", `**Cuma**`)
+        .replace("Saturday", `**Cumartesi**`)
+        .replace("Sunday", `**Pazar**`)
+        .replace("January", `**Ocak**`)
+        .replace("February", `**Şubat**`)
+        .replace("March", `**Mart**`)
+        .replace("April", `**Nisan**`)
+        .replace("May", `**Mayıs**`)
+        .replace("June", `**Haziran**`)
+        .replace("July", `**Temmuz**`)
+        .replace("August", `**Ağustos**`)
+        .replace("September", `**Eylül**`)
+        .replace("October", `**Ekim**`)
+        .replace("November", `**Kasım**`)
+        .replace("December", `**Aralık**`)
+        const uembed = new Discord.RichEmbed()
+        .setAuthor(user.tag, userinfo.avatar)
+        .setThumbnail(userinfo.avatar)
+        .setTitle('Kullanıcı;')
+        .addField(`Şu anda oynadığı oyun`, userinfo.od1, false)
+        .addField(`Durum`, userinfo.status, false)
+        .setColor('03f2df')
+        .addField(`Katılım Tarihi (Sunucu)`, userinfo.dctarihkatilma, false)
+        .addField(`Katılım Tarihi (Discord)`, userinfo.dctarih, false)
+        .addField(`Kimlik:`, userinfo.id, true)
+        .addField(`Botmu:`, userinfo.bot, true)
+        .addField(`Roller:`, `${msg.guild.members.get(user.id).roles.filter(r => r.name !== "@everyone").map(r => r).join(' **|** ') || "**Bu kullanıcıda hiçbir rol bulunmuyor**"}`, false)
+        .addField(`Son gönderdiği mesaj:`, userinfo.sonmesaj, false)
+        .setFooter(`${botadi} || Kullanıcı Sistemi`)
+        msg.channel.send(uembed)
     }
-
-    const member = message.guild.member(user);
-       
-    const embed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-    
-    .setThumbnail(user.avatarURL)
-    
-    .setTitle(`${user.username}#${user.discriminator} Kullanıcı Bilgi'si`)
-    
-    .addField("İsim :",`${user.username}#${user.discriminator}`, true)
-    .addField("İd :", `${user.id}`, true)
-    .addField("Discord Tag :", `#${user.discriminator}`, true)
-    .addField("Hesap Oluşturma Tarihi :", `${moment.utc(user.createdAt).format('dddd, MMMM.Do.YYYY, ')}`, true)
-    .addField("Sunucuya Katılma Tarihi :", `${moment.utc(member.joinedAt).format('dddd, MMMM.Do.YYYY')}`, true)
-    .addField("Durumu :", `${user.presence.status}`, true)
-  .setImage('GİF/RESİM LİNK')
-    message.channel.send({embed});
-    }
-
 exports.conf = {
-  enabled: true, 
-  guildOnly: false,
-  aliases: ['profil-bilgi','profilbilgi','kullanıcı-bilgi','kullanıcıbilgi','k-bilgi','kbilgi','kb', 'profil'], 
-  permLevel: 0 
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
 };
-
 exports.help = {
-  name: 'kullanıcı-bilgi', 
-  description: 'Etiketlediğin / kendi profilin hakkında bilgi verir.',
-  usage: 'kullanıcı-bilgi' 
+  name: 'kullanıcı',
+  description: 'İstediğiniz kullanıcını bilgilerini gösterir.',
+  usage: 'kullanıcı'
 };
